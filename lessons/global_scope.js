a3cd5bc5421ebbd93a57c402d85c87e9ff791efc
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-10-28 12:36:35 +1100

    global scope

---
+++ lessons/global_scope.js
@@ -0,0 +1,7 @@
+let satellite = 'The Moon';
+let galaxy = 'The Milky Way';
+let stars = 'North Star';
+var callMyNightSky = () => {
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+};
+console.log(callMyNightSky());
