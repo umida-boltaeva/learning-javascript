cdc09f1c1009c305bb258709b4b42703bfe2906d
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-10-28 11:38:45 +1100

    function expression

---
+++ lessons/function_expression.js
@@ -0,0 +1,9 @@
+var plantNeedsWater = function (day) {
+    if (day === 'Wednesday') {
+        return true;
+    } else {
+        return false;
+    }
+};
+plantNeedsWater('Tuesday');
+console.log(plantNeedsWater('Tuesday'));
