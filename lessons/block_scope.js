fa0f6acf063d9660c59edf247f621c6129a2d4b7
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-10-28 12:51:12 +1100

    practising block scope independently

---
+++ lessons/block_scope.js
@@ -0,0 +1,6 @@
+// var logVisibleLightWaves = () => {
+//     const lightWaves = 'Moonlight';
+//     console.log(lightWaves);
+// }
+// logVisibleLightWaves();
+// console.log(lightWaves);
---
+++ lessons/block_scope2.js
@@ -0,0 +1,15 @@
+// var dailyWaterIntake = () => {
+//     let waterIntake = '3 litres';
+//     console.log(waterIntake);
+// }
+
+// dailyWaterIntake();
+// console.log(waterIntake);
+
+var dailyWaterIntake = () => {
+    console.log(waterIntake);
+}
+let waterIntake = '3 litres';
+
+dailyWaterIntake();
+console.log(waterIntake);
