e63b23828a5644bae54db3f5c28a93d29639bf97
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-10-29 11:29:07 +1100

    practising let and const

---
+++ lessons/arrays.js
@@ -0,0 +1,11 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+condiments[0] = 'Mayo';
+condiments = ['Mayo'];
+
+console.log(condiments);
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+utensils[3] = 'Spoon';
+utensils = ['Spoon'];
+
+console.log(utensils);
