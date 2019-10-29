5b92279e2af62c52f7e5feb187683d292c3e6d46
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-10-29 13:18:44 +1100

    working on nested loops

---
+++ lessons/nested_loops.js
@@ -0,0 +1,12 @@
+let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
+let tinasFollowers = ['Sam', 'Marta', 'Elle'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+    for (let j = 0; j < tinasFollowers.length; j++) {
+        if (bobsFollowers[i] === tinasFollowers[j]) {
+            mutualFollowers.push(bobsFollowers[i]);
+        }
+    }
+};
+console.log(mutualFollowers);
