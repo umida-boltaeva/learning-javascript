01a6a4463c1edfd77e6a0556db49a766d6edd506
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-10-29 11:43:43 +1100

    practising methods

---
+++ lessons/.pop()_method.js
@@ -0,0 +1,5 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+chores.pop();
+let removedItem = chores.pop();
+console.log(chores);
+console.log(removedItem);
