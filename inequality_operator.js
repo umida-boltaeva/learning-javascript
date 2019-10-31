72fac0369fa506398d4d7de81da54adbc9df9cce
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-01 10:05:44 +1100

    strict inequality operator

---
+++ inequality_operator.js
@@ -0,0 +1,21 @@
+// function testStrictNotEqual(val) {
+//     if (val !== 17) {
+//         return "Not Equal";
+//     }
+//     return "Equal";
+// }
+
+// testStrictNotEqual(17);
+
+// console.log(testStrictNotEqual(17));
+
+function testStrictNotEqual(val) {
+    if (val !== 'bob') {
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+testStrictNotEqual(17);
+
+console.log(testStrictNotEqual(17));
