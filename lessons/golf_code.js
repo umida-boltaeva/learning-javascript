6fdf49ff2cfe7708305e351de31cf46487cdd620
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-04 10:09:29 +1100

    new exercises on conditionals

---
+++ lessons/golf_code.js
@@ -0,0 +1,24 @@
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+
+    if (strokes === 1) {
+        return "Hole-in-one!";
+    } else if (strokes <= par - 2) {
+        return "Eagle";
+    } else if (strokes === par - 1) {
+        return "Birdie";
+    } else if (strokes === par) {
+        return "Par";
+    } else if (strokes === par + 1) {
+        return "Bogey";
+    } else if (strokes === par + 2) {
+        return "Double Bogey";
+    } else if (strokes >= par + 3) {
+        return "Go Home!";
+    } else {
+        return "Change Me";
+    }
+}
+
+golfScore(5, 9);
---
+++ lessons/switch.js
@@ -0,0 +1,18 @@
+function caseInSwitch(val) {
+    var answer = "";
+
+    switch (caseInSwitch) {
+        case 1:
+            answer = 'alpha';
+            break;
+        case 2:
+            answer = 'beta';
+            break;
+        case 3:
+            answer = 'gamma';
+            break;
+        case 4:
+            answer = 'delta';
+            break;
+    }
+}
