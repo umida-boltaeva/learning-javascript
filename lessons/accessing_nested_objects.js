8bc950208b19dda5e05b772dc5523072e3a203f5
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-07 11:48:42 +1100

    working on accessing nested objects

---
+++ lessons/accessing_nested_objects.js
@@ -0,0 +1,14 @@
+var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"];
+console.log(gloveBoxContents);
