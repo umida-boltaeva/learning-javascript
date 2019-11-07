b8ee4b0c04954d84792cdcad9e327f250e242fe9
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-07 22:59:19 +1100

    nesting for loops

---
+++ lessons/nesting_for_loops.js
@@ -0,0 +1,17 @@
+function multiplyAll(arr) {
+    var product = 1;
+
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            product = product * arr[i][j];
+        }
+    }
+    return product;
+}
+
+
+console.log(multiplyAll([
+    [1, 2],
+    [3, 4],
+    [5, 6, 7]
+]));
