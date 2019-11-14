779646684bcff88f6f758a08a6c03f83db280873
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-15 10:51:02 +1100

    Using Destructuring Assignment to Extract Values from Objects

---
+++ lessons/destructing_assignment.js
@@ -0,0 +1,13 @@
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+const {
+    today,
+    tomorrow
+} = HIGH_TEMPERATURES;
+
+console.log(today);
+console.log(tomorrow);
