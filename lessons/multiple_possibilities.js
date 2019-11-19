1ee3aeb74b0cbae666ef541eb5fb52ec16627547
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-19 16:41:34 +1100

    tested using flags

--- lessons/multiple_possibilities.js
+++ lessons/multiple_possibilities.js
@@ -1,6 +1,6 @@
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 
-let vowelRegex = /b[aeiou]g/;
+let vowelRegex = /[aeiou]/gi;
 
 let result = quoteSample.match(vowelRegex);
 
