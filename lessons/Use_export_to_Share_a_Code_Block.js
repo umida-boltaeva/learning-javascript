9f104b87c0f458b19648e3085818a35e18159870
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-19 14:15:18 +1100

    Use export to Share a Code Block

---
+++ lessons/Use_export_to_Share_a_Code_Block.js
@@ -0,0 +1,25 @@
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+};
+
+export {
+    uppercaseString
+};
+
+const lowercaseString = (string) => {
+    return string.toLowerCase();
+};
+
+export {
+    lowercaseString
+};
+
+//another version is here:
+
+//  export const uppercaseString = (string) => {
+//     return string.toUpperCase();
+//   };
+
+//  export const lowercaseString = (string) => {
+//     return string.toLowerCase();
+//   };
