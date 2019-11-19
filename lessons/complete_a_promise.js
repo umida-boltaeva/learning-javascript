83a70356092c82718408d39de453dbdf374210d2
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-19 14:38:55 +1100

    Complete a Promise with resolve and reject

---
+++ lessons/complete_a_promise.js
@@ -0,0 +1,10 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+
+    let responseFromServer;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
