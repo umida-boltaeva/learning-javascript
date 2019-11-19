817080aa11730930bfd5edeb8783ed4c673e5c6b
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-19 14:59:03 +1100

    Handle a Rejected Promise with catch

---
+++ lessons/Handle_a_Fulfilled_Promise_with_then.js
@@ -0,0 +1,13 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+
+    let responseFromServer = true;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+makeServerRequest.then(result => {
+    console.log(result);
+});
---
+++ lessons/Handle_a_Rejected_Promise_with_catch.js
@@ -0,0 +1,18 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+
+    let responseFromServer = false;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+// makeServerRequest.then(result => {
+//     console.log(result);
+// });
+
+makeServerRequest.catch(error => {
+    console.log(error);
+});
