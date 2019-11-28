91a233d47adc46465ea9ad054c4949c5c211633a
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-28 14:37:12 +1100

    Check For The Presence of an Element With indexOf()

---
+++ lessons/indexOf().js
@@ -0,0 +1,10 @@
+function quickCheck(arr, elem) {
+
+    if (arr.indexOf(elem) >= 0) {
+        return true;
+    }
+    return false;
+}
+
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
