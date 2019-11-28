b8c1684199c138271a93aa4a3f0aff9d49c7843f
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-11-28 14:03:30 +1100

    Basic Data Structures: Add Items Using splice()

---
+++ lessons/add_items_using_splice().js
@@ -0,0 +1,7 @@
+function htmlColorNames(arr) {
+    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond');
+    return arr;
+}
+
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
