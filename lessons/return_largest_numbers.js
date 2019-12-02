f43027107da1f5c4edce48ac2f1cd4a3d4c5a9a7
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-12-02 11:59:31 +1100

    Return Largest Numbers in Arrays

---
+++ lessons/return_largest_numbers.js
@@ -0,0 +1,25 @@
+function largestOfFour(arr) {
+    const largestNumber = [0, 0, 0, 0];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] > largestNumber[i]) {
+                largestNumber[i] = arr[i][j];
+            }
+        }
+    }
+    return largestNumber;
+}
+
+largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+]);
+
+console.log(largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+]));
