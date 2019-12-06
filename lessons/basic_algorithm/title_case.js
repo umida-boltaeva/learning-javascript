eb9fe86e64014f9a0c37a43d958efad08e71a04b
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-12-06 15:00:43 +1100

    using slice and splice methods

---
+++ lessons/basic_algorithm/title_case.js
@@ -0,0 +1,12 @@
+function titleCase(str) {
+    let sentence = str.toLowerCase().split(" ");
+    for (let i = 0; i < sentence.length; i++) {
+        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+        sentence.join(" ");
+    }
+    return sentence;
+}
+
+titleCase("I'm a little tea pot");
+
+console.log(titleCase("I'm a little tea pot"));
