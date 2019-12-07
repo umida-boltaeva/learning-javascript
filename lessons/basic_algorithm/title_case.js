6507c7fda66144a2ee011e4b7114b8bb5ea414fa
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-12-07 15:22:56 +1100

    solved title case problem

--- lessons/basic_algorithm/title_case.js
+++ lessons/basic_algorithm/title_case.js
@@ -2,9 +2,9 @@ function titleCase(str) {
     let sentence = str.toLowerCase().split(" ");
     for (let i = 0; i < sentence.length; i++) {
         sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
-        sentence.join(" ");
+
     }
-    return sentence;
+    return sentence.join(" ");
 }
 
 titleCase("I'm a little tea pot");
