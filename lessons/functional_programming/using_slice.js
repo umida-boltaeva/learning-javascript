e5e58b2e9c93110591baae3f14ad820603690c20
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2019-12-12 11:26:55 +1100

    Return Part of an Array Using the slice Method

---
+++ lessons/functional_programming/using_slice.js
@@ -0,0 +1,7 @@
+function sliceArray(anim, beginSlice, endSlice) {
+
+    return anim.slice(beginSlice, endSlice);
+
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
